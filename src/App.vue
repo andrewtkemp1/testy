12faa1f6b98329b8bb815591b79947a7a7a45aee
andrewtkemp@gmail.com <andrewtkemp@gmail.com>
2020-07-23 11:08:18 -0500

    latest

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+      <Weather />
+  </v-app>
+</template>
+
+<script>
+import Weather from './components/Weather';
+
+export default {
+  name: 'App',
+
+  components: {
+    Weather
+  }
+};
+</script>
---
+++ src/components/Weather.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-container fluid>
+    <v-row :align="alignment" :justify="justify">
+      <v-col cols="6" :align="alignment" :justify="justify">
+        <v-text-field
+          label="Enter a city and press enter (ex: Dallas)"
+          width="300"
+          v-model="searchedLocation"
+          @keydown.enter="searchDate"
+        ></v-text-field>
+        <v-btn large color="primary" @click="searchDate" class="mb-5">Search</v-btn>
+        <v-row v-if="loadingCities">
+          <v-col cols="2" offset="5">
+            <div class="loading_box">
+              <p>Loading...</p>
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+            </div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row v-if="!loadingCities" :align="alignment" :justify="justify">
+      <v-expansion-panels>
+        <v-expansion-panel
+          @click="details(weatherCity.woeid)"
+          @keydown.enter="details(weatherCity.woeid)"
+          v-for="weatherCity in weatherCityList"
+          :key="weatherCity.woeid"
+        >
+          <v-expansion-panel-header>
+            {{
+            weatherCity.title
+            }}
+          </v-expansion-panel-header>
+          <v-expansion-panel-content v-if="loading">
+            <div class="loading_box">
+              <p>Loading...</p>
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+            </div>
+          </v-expansion-panel-content>
+          <v-expansion-panel-content v-else>
+            <v-row :align="alignment" :justify="justify">
+              <v-col
+                cols="2"
+                v-for="weatherData in weatherCityInfo.consolidated_weather.slice(
+                    0,
+                    5
+                  )"
+                :key="weatherData.id"
+              >
+                <v-card class="ma-3 pa-6">
+                  <h4>{{ momentWeekday(weatherData.applicable_date) }} {{ momentDate(weatherData.applicable_date) }}</h4>
+                  <img
+                    :src="
+                        `https://www.metaweather.com/static/img/weather/${weatherData.weather_state_abbr}.svg`
+                      "
+                  />
+                  <p>
+                    <span>Max:</span>
+                    {{ Math.round(weatherData.min_temp) }}°C
+                  </p>
+                  <p>
+                    <span>Min:</span>
+                    {{ Math.round(weatherData.max_temp) }}°C
+                  </p>
+                  <p>
+                    <span>Humidity: {{ weatherData.humidity }}%</span>
+                  </p>
+                  <p>
+                    <span>Wind Speed:</span>
+                    {{ Math.round(weatherData.wind_speed) }} mph
+                  </p>
+                  <p>
+                    <span>Wind Direction:</span>
+                    {{ Math.round(weatherData.wind_direction) }}°
+                    {{ weatherData.wind_direction_compass }}
+                  </p>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </v-row>
+  </v-container>
+</template>
+<script>
+import axios from "axios";
+import moment from "moment";
+
+const cors = "https://cors-anywhere.herokuapp.com/";
+const url = "https://www.metaweather.com/";
+
+export default {
+  name: "Weather",
+  data() {
+    return {
+      searchedLocation: this.value,
+      weatherCityList: [],
+      weatherCityInfo: {},
+      loading: true,
+      loadingCities: false,
+      alignment: "center",
+      dense: false,
+      justify: "center",
+    };
+  },
+  methods: {
+    searchDate() {
+      this.loadingCities = true;
+      axios
+        .get(`${cors}${url}api/location/search/?query=` + this.searchedLocation)
+        .then((res) => {
+          this.weatherCityList = res.data;
+          this.loadingCities = false;
+        });
+    },
+    details(woeId) {
+      this.loading = true;
+      axios
+        .get(`${cors}${url}/api/location/` + woeId)
+        .then((res) => {
+          this.weatherCityInfo = res.data;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    momentWeekday(date) {
+      const day = moment(date).weekday();
+      if (day === 0) {
+        return "Sunday";
+      } else if (day === 1) {
+        return "Monday";
+      } else if (day === 2) {
+        return "Tuesday";
+      } else if (day === 3) {
+        return "Wednesday";
+      } else if (day === 4) {
+        return "Thursday";
+      } else if (day === 5) {
+        return "Friday";
+      } else if (day === 6) {
+        return "Saturday";
+      }
+    },
+    momentDate(date) {
+      return moment(date).format("MM/DD");
+    },
+  },
+};
+</script>
